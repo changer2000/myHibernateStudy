4067b461857eea253dc6b6a9543cbf74f0a6cc09
changer20001 <changer20001@sohu.com>
2013-06-01 13:07:28 +0800

    change log4j

--- src/org/hibernate/tutorial/dao/EventManager.java
+++ src/org/hibernate/tutorial/dao/EventManager.java
@@ -2,11 +2,15 @@ package org.hibernate.tutorial.dao;
 
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 import org.hibernate.tutorial.domain.Event;
 import org.hibernate.tutorial.util.HibernateUtil;
 
 public class EventManager {
+	private static Log logger = LogFactory.getLog(EventManager.class);
+	
 	public static void main(String[] args) {
 		EventManager mgr = new EventManager();
 		//if (args[0].equals("store")) {
@@ -17,6 +21,7 @@ public class EventManager {
 
 	private void createAndStoreEvent(String title, Date theDate) {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		logger.debug(">>>>>>>>>>>>>");
 		session.beginTransaction();
 		Event theEvent = new Event();
 		theEvent.setTitle(title);
